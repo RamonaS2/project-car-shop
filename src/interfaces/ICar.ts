d5f5024114b05c5aa3c206cd0d1aa0868a437ab7
RamonaS2 <bandeiraramona2@gmail.com>
2022-10-27 21:37:48 -0300

    adicionando interfaces Ivehicle, IModel, Icar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+const carZodSchema = vehicleZodSchema.extend({
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
+});
+
+export type ICar = z.infer<typeof carZodSchema>;
